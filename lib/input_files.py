a5f2b32c9638f16e19fc7c991ec9cf964a60a637
Graham Thomas <graham_thomas@intuit.com>
2019-09-12 11:53:46 -0700

    Added comments and implemented output feature

---
+++ lib/input_files.py
@@ -0,0 +1,61 @@
+import logging
+import os
+import re
+
+from lib.annotations import *
+
+logger = logging.getLogger("root")
+ACCEPTED_FILE_EXTENSIONS = ["py"]
+
+
+def get_files_to_parse(relative_path):
+    """Walks through given directory and returns all files with ending
+        with an accepted file extension
+
+    Arguments:
+        relative_path {string} -- path to pull files from recursively
+
+    Returns:
+        List<String> -- list of filenames with fullpath
+    """
+
+    files = []
+    filepath = os.path.realpath(relative_path)
+    if os.path.isfile(filepath):
+        files.append(filepath)
+    else:
+        for r, d, f in os.walk(filepath):
+            for file in f:
+                if not file.split(".")[-1] in ACCEPTED_FILE_EXTENSIONS:
+                    continue
+                full_file_path = os.path.join(r, file)
+                logger.debug("Inspecting %s", file)
+                files.append(os.path.join(r, full_file_path))
+    return files
+
+
+def find_apidoc_annotations(text):
+    annotations = re.findall("@api.*", text)
+    return annotations
+
+
+def parse_generic_annotation(annotation):
+    """Parse the name at the beginning of each annotation and build objs based on that name
+
+    Arguments:
+        annotation {string} -- apidoc compliant string
+
+    Returns:
+        ApidocAnnotation -- Returns the class matching the annotation name
+    """
+
+    class_name = "A" + annotation.split(" ")[0][2:]  # Remove the @ at the beginning and capitalize
+    target_class = globals().get(class_name)
+    if not target_class:
+        logger.error(
+            "Could not find class for %s. It is either incorrect or not supported yet.", class_name
+        )
+        return None
+
+    anno_obj = target_class(annotation)
+    return anno_obj
---
+++ lib/parse_config.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+
+def parse_apidoc_json(file_path):
+    """Parses the required apidoc.json file at the root of the user specified directory
+
+    Returns:
+        dict -- apidoc.json file after being json parsed
+    """
+
+    try:
+        apidoc_json = open(os.path.join(file_path, "apidoc.json"), "r").read()
+        apidoc_conf = json.loads(apidoc_json)
+        return apidoc_conf
+    except FileNotFoundError:
+        logger.error("Could not locate apidoc.json in %s", file_path)
+        exit(1)
+    except json.decoder.JSONDecodeError:
+        logger.error("apidoc.json Invalid JSON!")
+        exit(1)
+
+
+def build_info_section(apidoc_conf):
+    """Converts apidoc.json to swagger configuration
+
+    Arguments:
+        apidoc_conf {dict} -- apidoc.json converted to a dictionary
+
+    Returns:
+        dict -- dictionary matching the require infomation section for swagger
+    """
+    info_section = {}
+    info_section["title"] = apidoc_conf.get("title") or apidoc_conf.get("name")
+    info_section["version"] = apidoc_conf.get("version", "0.0.0")
+    info_section["description"] = apidoc_conf.get("description")
+    return info_section
